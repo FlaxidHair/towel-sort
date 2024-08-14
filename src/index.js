865c74583c602935d54411fdef291bd9b2f5212c
Flaxid Hair <kmarlou@icloud.com>
2024-08-14 10:57:33 +0300

    feat: end function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    matrix.reduce((acc, el, i) => {
+        return matrix.concat(!(i % 2) ? el : el.reverse());
+    }, []);
+
+    return matrix.reduce((acc, el, i) => {
+        el.sort((a, b) => (!(i & 1) ? a - b : b - a)).map((e) => acc.push(e));
+        return acc;
+    }, []);
+};
